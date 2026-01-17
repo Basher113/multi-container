8110f02a70f62d654403d15591b863d9b34ad454
Basher113 <147272612+Basher113@users.noreply.github.com>
2026-01-17 09:37:36 +0800

    Fix client folder

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-
-});
-
-test('renders learn react link 2', () => {
-  
-});
+test('renders learn react link', () => {});
